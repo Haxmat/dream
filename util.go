df878b7dfa5d75aedab85418ef689e83b06077ff
Rinzen Necroforger <Necroforger@gmail.com>
2017-05-17 15:53:05 -0400

    Copied creation time function from floretta's selfbot
    https://github.com/Moonlington/FloSelfbot/blob/master/commands/

--- util.go
+++ util.go
@@ -1,6 +1,11 @@
 package dream
 
-import "github.com/Necroforger/discordgo"
+import (
+	"strconv"
+	"time"
+
+	"github.com/Necroforger/discordgo"
+)
 
 /*
 
@@ -20,4 +25,16 @@ func StatusColor(status discordgo.Status) int {
 		color = 0xffff00
 	}
 	return color
+}
+
+// CreationTime returns the creation time of a Snowflake ID relative to the creation of Discord.
+// Taken from https://github.com/Moonlington/FloSelfbot/blob/master/commands/commandutils.go#L117
+func CreationTime(ID string) (t time.Time, err error) {
+	i, err := strconv.ParseInt(ID, 10, 64)
+	if err != nil {
+		return
+	}
+	timestamp := (i >> 22) + 1420070400000
+	t = time.Unix(timestamp/1000, 0)
+	return
 }
